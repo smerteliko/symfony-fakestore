4e6aa1077e6c94c3fc51f3fbf615e61559f68e57
smerteliko <smerteliko@gmail.com>
2024-04-30 22:43:04 +0300

    Created addition for Category logic

---
+++ src/Entity/SubCategory.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubCategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SubCategoryRepository::class)]
+class SubCategory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'subCategories')]
+    private ?Category $Category = null;
+
+    /**
+     * @var Collection<int, Product>
+     */
+    #[ORM\OneToMany(targetEntity: Product::class, mappedBy: 'subCategory')]
+    private Collection $Product;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    public function __construct()
+    {
+        $this->Product = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->Category;
+    }
+
+    public function setCategory(?Category $Category): static
+    {
+        $this->Category = $Category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProduct(): Collection
+    {
+        return $this->Product;
+    }
+
+    public function addProduct(Product $product): static
+    {
+        if (!$this->Product->contains($product)) {
+            $this->Product->add($product);
+            $product->setSubCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): static
+    {
+        if ($this->Product->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getSubCategory() === $this) {
+                $product->setSubCategory(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
---
+++ src/Repository/SubCategoryRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\SubCategory;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<SubCategory>
+ *
+ * @method SubCategory|null find($id, $lockMode = null, $lockVersion = null)
+ * @method SubCategory|null findOneBy(array $criteria, array $orderBy = null)
+ * @method SubCategory[]    findAll()
+ * @method SubCategory[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SubCategoryRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SubCategory::class);
+    }
+
+	public function newSubCategory($data): void {
+		$subCategory = new SubCategory();
+		$subCategory->setName($data['Name']);
+		$subCategory->setCategory($data['Category']);
+
+		$subCategory->setCreatedAt(new \DateTimeImmutable());
+
+		$this->getEntityManager()->persist($subCategory);
+		$this->getEntityManager()->flush();
+	}
+
+    //    /**
+    //     * @return SubCategory[] Returns an array of SubCategory objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('s.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?SubCategory
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
